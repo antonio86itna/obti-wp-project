eadbff63dc56b865b21c841485206d0131ae4f71
antonio86itna <48077495+antonio86itna@users.noreply.github.com>
2025-08-16 00:31:49 +0200

    Add Customizer settings for social links and API tokens

---
+++ wp-content/plugins/obti-elementor-widgets/widgets/class-obti-chatbot.php
@@ -0,0 +1,20 @@
+<?php
+namespace OBTI_EW;
+
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+use Elementor\Widget_Base;
+
+class Chatbot extends Widget_Base {
+    public function get_name(){ return 'obti-chatbot'; }
+    public function get_title(){ return __('OBTI Chatbot','obti'); }
+    public function get_icon(){ return 'eicon-chat'; }
+    public function get_categories(){ return ['obti']; }
+
+    protected function render(){
+        $api_key = get_theme_mod('chatbot_api_key');
+        ?>
+        <div id="obti-chatbot" data-api-key="<?php echo esc_attr($api_key); ?>"></div>
+        <?php
+    }
+}
