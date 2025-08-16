af8afcd1df240662c364195835ba90e294ecde72
antonio86itna <48077495+antonio86itna@users.noreply.github.com>
2025-08-16 12:09:00 +0200

    chore: include plugins in tailwind scan

--- wp-content/themes/obti/tailwind.config.js
+++ wp-content/themes/obti/tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   content: [
     "./**/*.php",
-    "../../plugins/obti-elementor-widgets/**/*.{php,js}",
-    "../../plugins/obti-booking/**/*.{php,js}"
+    "../../plugins/**/*.{php,js}"
   ],
   theme: {
     extend: {
